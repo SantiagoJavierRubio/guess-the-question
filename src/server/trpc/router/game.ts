b62683c592a85267c5bed9331d2c7001a99d64b2
Santiago Javier Rubio <s.rubio91@gmail.com>
2022-12-30 16:36:14 -0300

    set score

--- src/server/trpc/router/game.ts
+++ src/server/trpc/router/game.ts
@@ -2,18 +2,29 @@ import { router, publicProcedure } from "../trpc";
 import { z } from "zod";
 import { env } from "../../../env/server.mjs";
 
+interface scoreResponse {
+    score: number
+}
+
+function isCorrectScore(score: unknown): score is scoreResponse {
+    return (typeof score === 'object' && typeof (score as scoreResponse).score === 'number')
+}
+
 export const gameRouter = router({
   evaluateAnswer: publicProcedure
     .input(z.object({ answer: z.string(), originalQuestion: z.string() }))
     .mutation(async ({ ctx, input }) => {
-        const score = await fetch(env.SS_API_URL, {
+        const response = await fetch(env.SS_API_URL, {
             method: "POST",
             body: JSON.stringify({
                 authorization_token: env.SS_TOKEN,
                 original: input.originalQuestion,
                 answer: input.answer
             })
-        })
-        return score.json();
+        });
+        const score = await response.json();
+        if(isCorrectScore(score)) {
+            return score.score * 100
+        } else return 0
     })
 });
